f1ec2f3b5930e0c5902084e2d8bec731ea9ef007
Aditya Raj <aadityaraj5511@gmail.com>
2025-07-04 10:08:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (31.51%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.find(needle)!= std::string::npos){
+        if(haystack.find(needle)!= string::npos){
             return haystack.find(needle);
         }
         else
