e02f43aecf81c034ff07c4276c044964506ddb65
Aditya Raj <aadityaraj5511@gmail.com>
2025-09-27 11:00:32 +0530

    Time: 12 ms (31.41%) | Memory: 16.7 MB (80.15%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,22 +1,54 @@
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
+// binary search approach.
+    int bs(vector<int>& nums, int start , int x ){
+        int end = nums.size()-1;
+        while(start <= end){
+            int mid = (start + end) /2;
+            if(nums[mid] == x){
+                return mid;
+            }
+            else if (nums[mid] < x){
+                start = mid+1;
+
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    int findPairs(vector<int>& nums, int k){
         sort(nums.begin(),nums.end());
-        int i = 0, j = 1 ;
         set<pair<int,int>> ans;
-        while(j<nums.size()){
-            if(nums[j]-nums[i] == k){
-                ans.insert({nums[i],nums[j]});
-                i++,j++;
+        for(int i =0; i<nums.size();i++){
+            if(bs(nums , i+1, nums[i]+k) != -1){
+                ans.insert({nums[i],nums[i]+k});
             }
-            else if(nums[j]-nums[i]>k){
-                i++;
-            }
-            else{
-                j++;
-            }
-            if(i==j) j++;
         }
         return ans.size();
     }
+
+
+// 2 pointer approach.
+    // int findPairs(vector<int>& nums, int k) {
+    //     sort(nums.begin(),nums.end());
+    //     int i = 0, j = 1 ;
+    //     set<pair<int,int>> ans;
+    //     while(j<nums.size()){
+    //         if(nums[j]-nums[i] == k){
+    //             ans.insert({nums[i],nums[j]});
+    //             i++,j++;
+    //         }
+    //         else if(nums[j]-nums[i]>k){
+    //             i++;
+    //         }
+    //         else{
+    //             j++;
+    //         }
+    //         if(i==j) j++;
+    //     }
+    //     return ans.size();
+    // }
 };
