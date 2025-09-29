482760475820108f78df7abf8be23f18bfd9c10e
Aditya Raj <aadityaraj5511@gmail.com>
2025-09-29 12:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (81.58%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        if(haystack.find(needle)!= string::npos){
-            return haystack.find(needle);
+     int strStr(string haystack, string needle){
+        int n = haystack.size();
+        int m = needle.size();
+        for(int i = 0 ; i <= n-m; i++){
+            for(int j = 0; j< m; j++){
+                if(needle[j] != haystack[i+j]){
+                    break;
+                }
+                if(j == m - 1){
+                    return i;
+                }
+            }
         }
-        else
         return -1;
-    }
+     }
+
+
+
+    // int strStr(string haystack, string needle) {
+    //     if(haystack.find(needle)!= string::npos){
+    //         return haystack.find(needle);
+    //     }
+    //     else
+    //     return -1;
+    // }
 };
