e000a05f0a5955c1f750e56822eeb781d82faeb3
Aditya Raj <aadityaraj5511@gmail.com>
2025-10-09 16:51:36 +0530

    Time: 3 ms (32.65%) | Memory: 14 MB (11.65%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto ele : nums){
+            mp[ele]++;
+        }
+        for(auto it : mp){
+            if(it.second == 1){
+                return it.first;
+            }
+        }
+        return {};
+    }
+};
