888bb33dabc32abe0a4cc8fe5848a7e3e38abc91
Aditya Raj <aadityaraj5511@gmail.com>
2025-10-10 15:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (91.93%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,45 +3,56 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         int s = 0;
-        int e = n - 1;
-        int mid;
+        int e = n-1;
+        int mid = s +(e-s)/ 2;
 
-        while (s <= e) {
-            mid = s + (e - s) / 2;
+        while(s<=e){
 
-            // if single element found
-            if (s == e) {
+            if(s==e){
                 return nums[s];
             }
 
-            // single element check
-            if ((mid - 1 >= 0 && nums[mid] != nums[mid - 1]) &&
-                (mid + 1 < n && nums[mid] != nums[mid + 1])) {
-                return nums[mid];
+            int currval = nums[mid];
+            int leftval = -1; // val at mid-1
+            if(mid-1 >=0){
+                leftval = nums[mid-1];
+            }
+            int rightval = -1; //val at mid+1.
+            if(mid+1<n){
+                rightval = nums[mid+1];
             }
 
-            // handle left duplicate
-            if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
-                int firststartindx = mid - 1;
 
-                // if left pair starts at odd index
-                if (firststartindx % 2 == 1) {
-                    e = firststartindx - 1;
-                } else {
-                    s = mid + 1;
+
+            //dublicate dose not exists.
+            if(currval != leftval && currval != rightval){
+                return currval;
+            }
+            //dublicate exists on left index.
+            if(currval == leftval && currval != rightval){
+                int firststartindx = mid-1;
+                if(firststartindx & 1){
+                    e = mid -1;
                 }
+                else{
+                    s = mid+1;
+                }
+
             }
-            // handle right duplicate
-            else if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
+            //dublicate exists on right index.
+            else if(currval != leftval && currval == rightval){
                 int firststartindx = mid;
-
-                if (firststartindx % 2 == 1) {
-                    e = firststartindx - 1;
-                } else {
-                    s = mid + 2;
+                if(firststartindx & 1){
+                    e = mid -1;
                 }
+                else{
+                    s = mid+1;
+                }
+
             }
+            mid =s + (e-s)/ 2;
         }
-        return -1; 
+    return -1;
+        
     }
 };
